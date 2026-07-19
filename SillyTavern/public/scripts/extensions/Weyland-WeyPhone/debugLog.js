7900a46c19e784c5f61cdf2faa87849314332b18
FFFox-ST-Manager <commanderbrainstorm@gmail.com>
2026-07-19 14:51:28 -0400

    Delete SillyTavern/public/scripts/extensions/Weyland-WeyPhone directory

--- SillyTavern/public/scripts/extensions/Weyland-WeyPhone/debugLog.js
+++
@@ -1,9 +0,0 @@
-export const EXCLUDED_CHARACTER_NAMES = ['Weybot', 'Mirror Weyland', 'Kinsbane Manor'];
-
-/**
- * @param {Array<{name: string}>} characters SillyTavern's context.characters
- * @param {string[]} [excludedNames]
- */
-export function getSelectableCharacters(characters, excludedNames = EXCLUDED_CHARACTER_NAMES) {
-    return characters.filter(character => !excludedNames.includes(character.name));
-}
